1044e00d36d9aea7b7d83c152232082b4836d95c
ahlraf <72507078+ahlraf@users.noreply.github.com>
2021-06-20 04:12:29 +0530

    crispy forms, text video page

--- teamshome/tests.py
+++ teamshome/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView 
+from .views import HomePageView, TextVideoPageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -27,4 +27,31 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+
+class TextVideoPageTests(SimpleTestCase): # new
+
+    def setUp(self):
+        url = reverse('textvideo_about')
+        self.response = self.client.get(url)
+
+    def test_textvideopage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_textvideopage_template(self):
+        self.assertTemplateUsed(self.response, 'textvideo_about.html')
+
+    def test_textvideopage_contains_correct_html(self):
+        self.assertContains(self.response, 'Text and Video Call')
+
+    def test_textvideopage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+    def test_textvideopage_url_resolves_textvideopageview(self):
+        view = resolve('/textvideo/')
+        self.assertEqual(
+            view.func.__name__,
+            TextVideoPageView.as_view().__name__
         )
--- teamshome/urls.py
+++ teamshome/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, TextVideoPageView
 
 urlpatterns = [
+    path('textvideo/', TextVideoPageView.as_view(), name='textvideo_about'),
     path('', HomePageView.as_view(), name='home'),
 ]
