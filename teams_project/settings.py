43be4fff5762bf6fe1879d5d4d2255fb4c62d3ff
ahlraf <72507078+ahlraf@users.noreply.github.com>
2021-06-20 04:38:07 +0530

    adding allauth

--- teams_project/settings.py
+++ teams_project/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'crispy_forms',
+    'allauth', 
+    'allauth.account',
 
     'users.apps.UsersConfig',
     'teamshome.apps.TeamshomeConfig',
@@ -139,7 +142,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-LOGIN_REDIRECT_URL = 'home' 
-LOGOUT_REDIRECT_URL = 'home' 
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+LOGIN_REDIRECT_URL = 'home' 
+ACCOUNT_LOGOUT_REDIRECT = 'home' 
+
+# django-allauth-config
+SITE_ID = 1
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend', 
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+
+ACCOUNT_SESSION_REMEMBER = True
